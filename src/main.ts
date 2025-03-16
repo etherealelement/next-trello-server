ab5bdba3516f989ecd3f86e71372bad185abd128
Vitaly Safonov <vitaliysafonov@livedune.com>
2025-03-16 16:37:53 +0300

    setup docker add env and dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { ConfigService } from "@nestjs/config";
+
+import * as cookieParser from "cookie-parser";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = app.get(ConfigService);
+
+  app.use(cookieParser(config.getOrThrow("COOKIES_SECRET")));
+
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
