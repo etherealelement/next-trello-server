0c0b10825a81b6fbbb8ca0d2ae7bedc75a8ed519
Vitaly Safonov <vitaliysafonov@livedune.com>
2025-03-19 19:18:50 +0300

    config docker and add base auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,39 @@
-import { Controller } from '@nestjs/common';
-import { AuthService } from './auth.service';
+import {
+  Controller,
+  Body,
+  Post,
+  HttpCode,
+  HttpStatus,
+  Req,
+  Res,
+} from "@nestjs/common";
+import { AuthService } from "./auth.service";
+import { RegisterDto } from "./domain/dto/register-dto";
+import { Request } from "express";
+import { LoginDto } from "./domain/dto/login-dto";
 
-@Controller('auth')
+@Controller("auth")
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  public constructor(private readonly authService: AuthService) {}
+
+  @Post("register")
+  @HttpCode(HttpStatus.OK)
+  public async register(@Req() req: Request, @Body() dto: RegisterDto) {
+    return this.authService.register(req, dto);
+  }
+
+  @Post("login")
+  @HttpCode(HttpStatus.OK)
+  public async login(@Req() req: Request, @Body() dto: LoginDto) {
+    return this.authService.login(req, dto);
+  }
+
+  @Post("logout")
+  @HttpCode(HttpStatus.OK)
+  public async logout(
+    @Req() req: Request,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    return this.authService.logout(req, res);
+  }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,95 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+  UnauthorizedException,
+} from "@nestjs/common";
+import { RegisterDto } from "./domain/dto/register-dto";
+import { UserService } from "../user/user.service";
+import { AuthMethod, User } from "../../prisma/__generated__";
+import { Request, response } from "express";
+import { LoginDto } from "./domain/dto/login-dto";
+import { verify } from "argon2";
+import { ConfigService } from "@nestjs/config";
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  public constructor(
+    private readonly userService: UserService,
+    private readonly configService: ConfigService,
+  ) {}
+
+  public async register(req: Request, dto: RegisterDto) {
+    const isExist = await this.userService.findByEmail(dto.email);
+
+    if (isExist) {
+      throw new ConflictException(
+        "Регистрация не удалась, пользователь с таким email уже существует.",
+      );
+    }
+
+    const newUser = await this.userService.create(
+      dto.email,
+      dto.password,
+      dto.name,
+      AuthMethod.CREDENTIALS,
+      "s",
+      false,
+    );
+
+    return this.saveSession(req, newUser);
+  }
+  public async login(req: Request, dto: LoginDto) {
+    const user = await this.userService.findByEmail(dto.email);
+
+    if (!user || !user.password) {
+      throw new NotFoundException("Пользователь не найден");
+    }
+
+    const isValidPassword = await verify(user.password, dto.password);
+
+    if (!isValidPassword) {
+      throw new UnauthorizedException(
+        "Неверный пароль. Пожалуйста, попробуйте еще раз или восстановите пароль, если забыли его",
+      );
+    }
+
+    return this.saveSession(req, user);
+  }
+  public async logout(req: Request, res: Response): Promise<void> {
+    return new Promise((resolve, reject) => {
+      req.session.destroy((err) => {
+        if (err) {
+          return reject(
+            new InternalServerErrorException(
+              "Не удалось завершить сессию. Возможно возникла проблема с сервером или сессия уже была завершена",
+            ),
+          );
+        }
+
+        response.clearCookie(this.configService.getOrThrow("SESSION_NAME"));
+        resolve();
+      });
+    });
+  }
+
+  private async saveSession(req: Request, user: User) {
+    return new Promise((resolve, reject) => {
+      req.session.userId = user.id;
+      req.session.save((err) => {
+        if (err) {
+          return reject(
+            new InternalServerErrorException(
+              "Не удалось сохранить сессию. Проверьте правильно ил настроены параметры сессии",
+            ),
+          );
+        }
+
+        resolve({
+          user,
+        });
+      });
+    });
+  }
+}
